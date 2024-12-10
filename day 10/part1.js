7e85153c42947b6c98667cfccd0e6dd68fedc22a
unkobweb <sieg.alexandre@gmail.com>
2024-12-10 18:54:59 +0100

    day 10

---
+++ day 10/part1.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('./input.txt', 'utf-8').split('\r\n').map(line => line.split('').map(Number));
+
+const allZeros = []
+
+for (let y = 0; y < input.length; y++) {
+  for (let x = 0; x < input[y].length; x++) {
+    if (input[y][x] === 0) {
+      allZeros.push([x, y]);
+    }
+  }
+}
+
+let nineCount = 0;
+
+for (const zero of allZeros) {
+  const knownNine = new Set();
+
+  function findPossibleNext(x, y, currentValue) {
+    if (input[y][x] === 9) {
+      knownNine.add(`${x},${y}`);
+      return;
+    }
+    const possibleNext = [];
+    if (input[y][x + 1] === currentValue + 1) findPossibleNext(x + 1, y, currentValue + 1);
+    if (input[y][x - 1] === currentValue + 1) findPossibleNext(x - 1, y, currentValue + 1);
+    if (input[y + 1] && input[y + 1][x] === currentValue + 1) findPossibleNext(x, y + 1, currentValue + 1);
+    if (input[y - 1] && input[y - 1][x] === currentValue + 1) findPossibleNext(x, y - 1, currentValue + 1);
+    return possibleNext;
+  }
+
+  const [x, y] = zero;
+  findPossibleNext(x, y, 0);
+  nineCount += knownNine.size;
+}
+
+console.log(nineCount);
